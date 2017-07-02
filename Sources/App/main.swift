617c618476096ddf2bf4faf5941455711b96b25a
Tito Sacchi <tito.sakki@gmail.com>
2017-07-02 14:39:02 +0200

    Add the top part of the main page and split leaf files

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -6,7 +6,7 @@ try config.addProvider(LeafProvider.Provider.self)
 let drop = try Droplet(config)
 
 drop.get { request in
-    return try drop.view.make("index.leaf")
+    return try drop.view.make("index.leaf", ["user": ["name": "[Username]", "realname": "[Real name]"]])
 }
 
 try drop.run()
